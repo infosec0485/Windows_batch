94d16c60e4ca196771dfb34d1568f46cbdd8e2d2
Nerd_YJH <71858329+infosec0485@users.noreply.github.com>
2023-04-07 00:33:17 +0900

    Update policy.bat

--- policy.bat
+++ policy.bat
@@ -57,7 +57,7 @@ net accounts /minpwlen:9
 net accounts /forcelogoff:5
 
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v ScreenSaveActive /t REG_SZ /d 1 /f
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v ScreenSaveTimeOut /t REG_SZ /d 300 /f
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v ScreenSaveTimeOut /t REG_SZ /d 1200 /f
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v ScreenSaverIsSecure /t REG_SZ /d 1 /f
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v SCRNSAVE.EXE /t REG_SZ /d "C:\WINDOWS\system32\scrnsave.scr" /f
 
